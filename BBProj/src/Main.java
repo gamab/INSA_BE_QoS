5fefd83ae6ded3e567f360f8e42db7010734e868
Gamab <gabi.mabs@gmail.com>
2015-05-25 00:47:54 +0200

    [Gab] Add a test to check if the ressource acceptance works or not

--- BBProj/src/Main.java
+++ BBProj/src/Main.java
@@ -1,28 +1,134 @@
 import java.net.Inet4Address;
 import java.net.UnknownHostException;
 
+import routerconf.RouterDescriptor;
+import routerconf.RouterRSRVTable;
 import routerconf.TelnetRouterClient;
+import Log.Log;
+import flow.FlowDescriptor;
 
 public class Main {
+	public static String TAG = "Main";
 
 	public static void main(String args[]){
+		testFlowRSRV();
+	}
+
+	public static void testTelnet() {
 		Inet4Address ipRtr;
 		try {
 			ipRtr = (Inet4Address) Inet4Address.getByName("localhost");
 
 			TelnetRouterClient trc = new TelnetRouterClient(ipRtr,23, "rtr", "mdp");
-			
+
 			trc.sendCommand("ls");
-			
+
 			trc.disconnect();
-			
+
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 
+	public static void testFlowRSRV() {
+		try {
+			byte [] ipRtr = new byte[] {(byte)127,(byte)0,(byte)0,(byte)1};
+			byte [] idRtr = new byte[] {(byte)101,(byte)101,(byte)101,(byte)101};
+			byte [] prefix = new byte[] {(byte)192,(byte)168,(byte)1,(byte)0};
+			int mask = 24;
+			float availRess = 100.0f;
+
+			RouterDescriptor rtrD = new RouterDescriptor( 
+					(Inet4Address)Inet4Address.getByAddress(idRtr),
+					(Inet4Address)Inet4Address.getByAddress(ipRtr), 
+					(Inet4Address)Inet4Address.getByAddress(prefix),mask,
+					availRess);
+			Log.d(TAG,"Created router descriptor " + rtrD);
+
+			byte [] ipSrc = new byte[] {(byte)192,(byte)168,(byte)0,(byte)2};
+			byte [] ipDst1 = new byte[] {(byte)192,(byte)168,(byte)1,(byte)3};
+			byte [] ipDst2 = new byte[] {(byte)192,(byte)168,(byte)1,(byte)3};
+			float transmRate = 64.0f;
+			//Inet4Address ipSrc, Inet4Address ipDst, int pSrc,int pDst, float transmRate, int proto
+			FlowDescriptor fd1 = new FlowDescriptor(
+					(Inet4Address)Inet4Address.getByAddress(ipSrc),
+					(Inet4Address)Inet4Address.getByAddress(ipDst1), 
+					1340,
+					1342,
+					transmRate,
+					0x80);
+			FlowDescriptor fd2 = new FlowDescriptor(
+					(Inet4Address)Inet4Address.getByAddress(ipSrc),
+					(Inet4Address)Inet4Address.getByAddress(ipDst2), 
+					1345,
+					1347,
+					transmRate,
+					0x80);
+
+			RouterRSRVTable rrsrvt = new RouterRSRVTable(rtrD);
+
+			boolean flow1Accepted = false;
+			boolean flow2Accepted = false;
+			if (rrsrvt.isFlowAcceptable(fd1)) {
+				Log.d(TAG,"We can accept flow 1 " + fd1);
+				if (rrsrvt.acceptFlow(fd1)) {
+					Log.d(TAG,"Flow 1 " + fd1 + " accepted");
+					Log.d(TAG,"Router descriptor is now " + rtrD);
+					flow1Accepted=true;
+				}
+				else {
+					Log.e(TAG,"Flow 1 " + fd1 + " refused");					
+				}
+			}
+			else {
+				Log.e(TAG,"We cannot accept Flow 1 " + fd1);					
+			}
+
+			if (flow1Accepted && rrsrvt.isFlowAcceptable(fd2)) {
+				Log.e(TAG,"We can accept flow 2 " + fd2);
+				if (rrsrvt.acceptFlow(fd1)) {
+					Log.e(TAG,"Flow 2 " + fd2 + " accepted");
+					Log.e(TAG,"Router descriptor is now " + rtrD);
+					flow2Accepted=true;
+				}
+				else {
+					Log.d(TAG,"Flow 2 " + fd2 + " refused");					
+				}
+			}
+			else {
+				Log.d(TAG,"We cannot accept Flow 2 " + fd2);	
+			}
+
+			if (flow1Accepted && !flow2Accepted) {
+				if (rrsrvt.freeFlowRSRV(fd1)) {
+					Log.d(TAG,"We could free Flow 1 " + fd1);	
+					Log.d(TAG,"Router descriptor is now " + rtrD);
+					if (rrsrvt.isFlowAcceptable(fd2)) {
+						Log.d(TAG,"We can accept flow 2 " + fd2);
+						if (rrsrvt.acceptFlow(fd1)) {
+							Log.d(TAG,"Flow 2 " + fd2 + " accepted");
+							Log.d(TAG,"Router descriptor is now " + rtrD);
+							flow2Accepted=true;
+						}
+						else {
+							Log.e(TAG,"Flow 2 " + fd2 + " refused");					
+						}
+					}
+					else {
+						Log.e(TAG,"We cannot accept Flow 2 " + fd2);	
+					}
+				}
+				else {
+					Log.e(TAG,"We could not free Flow 1 " + fd1);					
+				}
+			}
+
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 
 }
