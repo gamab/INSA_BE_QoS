46e28f5b27aace897b8111852acb2252e6eaaed1
Gamab <gabi.mabs@gmail.com>
2015-05-25 10:55:27 +0200

    [Gab] Ajout des débits de codecs

--- BBProj/src/ressources/Ressources.java
+++ BBProj/src/ressources/Ressources.java
@@ -7,9 +7,20 @@ package ressources;
 public class Ressources {
 	
 	/*
-	 * Codecs
-	 */
-	public static final int debit_G711 = 64;
-	
-	
+	 * Bandwith of codecs on Ethernet in kbps
+	 * http://www.cisco.com/c/en/us/support/docs/voice/voice-quality/7934-bwidth-consume.html
+	 * Regexp pour parser un CSV avec les deux colonnes : 
+	 * cat codec_rate.csv | sed 's/[.]/_/g' | sed 's/\([^,]*\)[,]\(.*\)/public static final float \1 = \2f;/g'
+	 */	
+	public static final float G_711 = 87_2f;
+	public static final float G_729 = 31_2f;
+	public static final float G_723_1a = 21_9f;
+	public static final float G_723_1b = 20_8f;
+	public static final float G_726_a = 55_2f;
+	public static final float G_726_b = 47_2f;
+	public static final float G_728 = 31_5f;
+	public static final float G722 = 87_2f;
+	public static final float ilbc_mode_2 = 38_4f;
+	public static final float ilbc_mode_3 = 28_8f;
+
 }
